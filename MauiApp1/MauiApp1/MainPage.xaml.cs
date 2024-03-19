4b765b2cae5a473805f28d9235aac9ab7d647f07
JoaoEGiovanne <joaogiovanneetec@gmail.com>
2024-03-19 12:39:22 -0300

    first commit

---
+++ MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button15Percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button30Percent_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundUp_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonRoundDowm_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PorcentageSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            TipPorcentagemLabel.Text = $"{PorcentageSlider.Value.ToString()}%";
+        }
+    }
+
+}
